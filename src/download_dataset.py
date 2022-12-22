2b2cab92739dff54a79859e9de9c1c25cd4bab6a
lukas machado <lukasmachado.developer@gmail.com>
2022-12-22 11:50:32 -0300

    att branch

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -11,7 +11,7 @@ def download(file:str = None, folder:str = None) -> None:
         commandS3 = "aws s3 cp s3://2021-204/2021-204-1-1/data/ {0} --recursive --exclude \"*\"  --include {1}  --endpoint-url https://s3.ki.se".format(folder,file)
         os.system(commandS3)
         elapsedTime = time.strftime("%H:%M:%S", time.gmtime(time.time() - startTime))
-        print("---> Tempo para baixar imagem {} : {}.\n",file, elapsedTime)
+        print("---> Tempo para baixar imagem {} : {}.\n".format(file, elapsedTime))
         
     #verificando
     if os.path.exists(folder+file): 
